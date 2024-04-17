3cd341ab4c1c3a7826390a3b87fca2c221650c29
iwebsteraudio <iwebster.audio@gmail.com>
2024-04-17 10:03:34 +0100

    Completes Kata 4 - removes some junk code

--- models/articles-models.js
+++ models/articles-models.js
@@ -5,12 +5,7 @@ const fetchArticleById = (article_id) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then(({ rows }) => {
-        
-      if (typeof article_id === Number) {
-        console.log(article_id)
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-      }
-
+    
       if (rows.length > 0) {
         return rows[0];
       }
